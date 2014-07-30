b2b02e63cbbbfc4e497388b950ed4809309dcabc
Marco Alvarez <okramuk@gmail.com>
2014-07-29 20:12:15 -0500

    pruebasubida

---
+++ Variables.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+/*void main()
+{
+	int tempInt = 0;
+	tempInt = 45;
+
+	float tempFloat = 0;
+	tempFloat = 32.65463321654654;
+
+	double tempDouble = 0;
+	tempDouble = 32.65463321654654;
+	cout << "My variable tempInt = " << tempInt << endl;
+	cout << "My variable tempFloat = " << tempFloat << endl;
+	cout << "My variable tempDoble = " << tempDouble << endl;
+}*/
